8bd7650be0b42731dc1a3379fa9abbda78647abf
Darian Camacho <delgadodarian2003@gmail.com>
2024-02-27 16:31:55 -0600

    Welcome Page Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },  
   { path: 'signup', component: SignupComponent },
+  { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
